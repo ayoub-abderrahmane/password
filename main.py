08eac76f4936c2d1c8e9a1d8928b3c2d688a0aa6
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2023-12-06 16:59:02 +0100

    job a finir

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from hashlib import sha256   # import getpass (pour masquer le mot de passe sur l'écran)
 import secrets
 import json
+
+
+stockage = {}
+
 while True:
+  name = str(input("Veuillez entrez votre nom d'utilisateur : "))
+
   password = str(input("Veuillez entrez votre mot de passe : "))
 
-  # Définition des conditions pour créer :  
+  # Définition des conditions pour créer un mot de passe robuste :  
 
   if len(password) < 8:
         print ("Le mot de passe doit contenir au moins 8 caractères")
@@ -34,19 +40,30 @@ while True:
   password = password.encode()
   password_hash = sha256(salt + password).hexdigest()
 
+  # Ajout du name et du mot de passse hashé dans une bibliotheque
+
+  stockage ["Name :"] = name
+  
+  stockage ["Mdp :"] = password_hash
+
+  if name == stockage ["Name :"]:
+      stockage["Mdp :"]
 
   # Ecriture du mot de passe hashé et salé dans un fichier
 
-  fichier = open('fichier.json','a')
+  with open('fichier.json', 'a') as f:
+      f.write(json.dumps(stockage , indent=2))
 
-  fichier.write(password_hash)
+
+  # Questions pour continuer le programmme et afficher les mdp
   
   to_continue= input ("Souhaitez-vous ajouter de nouveaux mots de passe ? (oui/non) : ")
 
-  afficher_mdp = input ("Souhaitez-vous ajouter de nouveaux mots de passe ? (oui/non) :")
+  afficher_mdp = input ("Souhaitez-vous afficher vos mots de passe ? (oui/non) : ")
 
   if afficher_mdp == "oui" or afficher_mdp == "Oui" or afficher_mdp == "OUI":
-      open('fichier.json')
- 
+      print (json.loads (stockage))
+
+  
   if to_continue == "non" or to_continue == "Non" or to_continue == "NON":
      break
---
+++ test.py
@@ -0,0 +1,4 @@
+import random
+test = "azertyuiopqsdfghjklmwxcvbn1234567890&!@#$*^"
+
+print (random.sample(test , 8))
